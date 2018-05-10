b1b4b02b908e414bcb7e5e572131461c6c31dcd7
rpgrange <rpgrange@ncsu.edu>
2018-05-10 00:10:41 -0400

    Added jumping and air strafing
    Currently the player can jump indefinetly and at anytime

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour {
 	private int count;
 	public float moveForce = 50;
     public float maxVelocity = 10;
+    public float jumpForce = 200;
 	public Text countText;
 	public Text winText;
 
@@ -24,13 +25,19 @@ public class PlayerController : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVerticle = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVerticle);
+        Vector3 ignore = new Vector3(0, rb.velocity.y, 0);
+        Vector3 newXZVelocity = rb.velocity + (movement * (moveForce * Time.deltaTime / rb.mass)) - ignore;
         Vector3 newVelocity = rb.velocity + (movement * (moveForce * Time.deltaTime / rb.mass));
-        if (newVelocity.magnitude < maxVelocity) {
+
+        if (newXZVelocity.magnitude < maxVelocity) {
             rb.AddForce(movement * moveForce);
         } else {
-            rb.velocity = newVelocity.normalized * maxVelocity;
-        } 
-	}
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Vector3 jumpVector = new Vector3(0.0f, jumpForce, 0.0f);
+            rb.AddForce(jumpVector);
+        }
+    }
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag ("collectable")) {
 			other.gameObject.SetActive (false);
